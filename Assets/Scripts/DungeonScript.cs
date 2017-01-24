dd961e632d26c216343408686be5187e8ecbb8e7
LENOVOPC\Patrick Luk <lukpwh330@yahoo.com.hk>
2017-01-23 21:40:13 -0500

    Camera now follows player.  Added simple cellular automata algorithm to generate random rooms.

--- Assets/Scripts/DungeonScript.cs
+++ Assets/Scripts/DungeonScript.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class DungeonScript : MonoBehaviour
 {
 
-	public float speed;	// speed of player
+	public float speed; // speed of player
+
 	public GameObject player; // the player
+	public GameObject wall; // wall tiles
+
 	////public Sprite[] spritePlayer = new Sprite[40];
 
-
+	// Use this for initialization
 	void Start ()
 	{
 		// initialize player object
@@ -18,9 +21,17 @@ public class DungeonScript : MonoBehaviour
 		player.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
 
 		// set speed off player
-		speed = 5.0f;
+		speed = 10.0f;
+
+		// create room
+		createRoom (100, 100);
+
+		// initialize camera
+		Camera.main.orthographicSize = 10;
+		Camera.main.transform.position = new Vector3 (0.0f, 0.0f, -10.0f);
 	}
 
+	// Update is called once per frame
 	void Update ()
 	{
 		// change of position of the player
@@ -45,5 +56,61 @@ public class DungeonScript : MonoBehaviour
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			player.transform.position += new Vector3 (horizontalMovement, 0.0f, 0.0f);
 		}
+
+		// update camera position
+		Camera.main.transform.position = new Vector3 (player.transform.position[0], player.transform.position[1], Camera.main.transform.position[2]);
 	}
+
+	// create a random room
+	void createRoom (int width, int height)
+	{
+		System.Random random = new System.Random ();
+		int[,] room = new int[width, height];
+
+		// initializing room
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				room [i, j] = random.NextDouble () < 0.5 ? 1 : -1;
+			}
+		}
+
+		// smoothen room
+		for (int count = 0; count < 10; count++) { // number of iterations
+			int[,] newRoom = new int[width, height]; // array for storing the smoothed room
+
+			// loop through each cell
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+
+					// count whether there are more air blocks/wall blocks in the 3x3 area
+					int wallCounter = 0;
+					for (int row = i - 1; row <= i + 1; row++) {
+						for (int col = j - 1; col <= j + 1; col++) {
+
+							// add to counter 
+							try {
+								wallCounter += room [row, col];
+							} catch (IndexOutOfRangeException) {}
+						}
+					}
+					newRoom [i, j] = (wallCounter >= 0) ? 1 : -1; // set the block to wall/air depending on the counter
+
+				}
+			}
+
+			room = newRoom; // update the room
+		}
+
+		// create room
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (room [i, j] == 1) {
+					// place walls
+					wall = (GameObject)Instantiate (wall, transform.position, transform.rotation);
+					wall.transform.position = new Vector3 (i - width / 2.0f, j - height / 2.0f, 0.0f);
+				}
+			}
+		}
+	}
+		
 }
