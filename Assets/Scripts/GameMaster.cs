e254f93212b1b6e7edd7bfd8038f4a74af36a385
Daniel Clarke <danclarke88@gmail.com>
2017-01-26 04:34:05 -0500

    Big commit - code for in class demo

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster Instance;
+
+    public int iceCount;
+    public int fireCount;
+
+	void Awake () {
+        if (Instance != null)
+            Destroy(Instance);
+        else
+            Instance = this;
+    }
+}
