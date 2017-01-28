390e2a70548ea93fcf931480f49faf4e00627292
LENOVOPC\Patrick Luk <lukpwh330@yahoo.com.hk>
2017-01-28 12:52:42 -0500

    Room now change appearance according to the fire and ice votes

--- Assets/Scripts/DungeonScript.cs
+++ Assets/Scripts/DungeonScript.cs
@@ -9,8 +9,12 @@ public class DungeonScript : MonoBehaviour
 	public float speed; // speed of player
 
 	public GameObject iceTiles; // ice tiles
+	public GameObject waterTiles; // water tiles
+	public GameObject steamTiles; // steam tiles
+	public GameObject floorTiles; // ground tiles
 	public GameObject lavaTiles; // lava tiles
 	public GameObject player; // the player
+	public GameObject borderTiles; // border tiles
 	public GameObject wallTiles; // wall tiles
 
 	////public Sprite[] spritePlayer = new Sprite[40];
@@ -19,12 +23,13 @@ public class DungeonScript : MonoBehaviour
 	private int air = -1; // DO NOT MODIFY
 	private int flood = 0;
 	private int wall = 1; // DO NOT MODIFY
-	private int fire = 2;
-	private int ice = 3;
+
+	private System.Random random; // random numnber generator
 
 	// Use this for initialization
 	void Start ()
 	{
+		random = new System.Random ();
 		int roomWidth = 96; // width of room
 		int roomHeight = 64; // height of room
 
@@ -32,7 +37,8 @@ public class DungeonScript : MonoBehaviour
 		speed = 10.0f;
 
 		// create room
-		createRoom (roomWidth, roomHeight, 3, 5);
+		int [,] randomRoom = generateRoomArray (roomWidth, roomHeight);
+		createRoom (randomRoom, 0, 0);
 
 		// initialize player object
 		player = (GameObject)Instantiate (player, new Vector3 (roomWidth / 2, roomHeight / 2, 0.0f), transform.rotation);
@@ -72,10 +78,9 @@ public class DungeonScript : MonoBehaviour
 		Camera.main.transform.position = new Vector3 (player.transform.position[0], player.transform.position[1], Camera.main.transform.position[2]);
 	}
 
-	// create a random room
-	void createRoom (int width, int height, int fireVotes, int iceVotes) // height of room, width of room, number of fire votes, number of ice votes
+	// generate an array containing the information of a room
+	int [,] generateRoomArray (int width, int height) // height of room, width of room
 	{
-		System.Random random = new System.Random ();
 		int[,] room = new int[width, height];
 
 		bool satisfied = false; // whether room is satisfied or not
@@ -113,7 +118,7 @@ public class DungeonScript : MonoBehaviour
 				for (int i = 0; i < width; i++) {
 					for (int j = 0; j < height; j++) {
 
-						// count whether there are more air blocks/wall blocks in the 3x3 area
+						// count whether there are more air tiles/wall tiles in the 3x3 area
 						int wallCounter = 0;
 						for (int row = i - 1; row <= i + 1; row++) {
 							for (int col = j - 1; col <= j + 1; col++) {
@@ -164,15 +169,75 @@ public class DungeonScript : MonoBehaviour
 			satisfied = satisfied || (touchBottomOrLeft && touchTopOrRight); // the exits are connected
 		} while (!satisfied);
 
+		return room;
+	}
+
+	void createRoom (int [,] room, int fireVotes, int iceVotes) { // array of the room, number of fire votes, number of ice votes
+		int width = room.GetLength(0); // width of dungeon;
+		int height = room.GetLength(1); // height of dungeon;
+		float xScale = 0.06f + 2.0f / Mathf.Min(width, height); // x scale of perlin noise
+		float xOffset, yOffset; // x and y offset of perlin noise;
+
+		// set temperature map of room;
+		float [,] temperature = new float[width, height];
+		xOffset = (float) (random.NextDouble ()) * 1000;
+		yOffset = (float) (random.NextDouble ()) * 1000;
+		float heightOffset = (fireVotes + iceVotes == 0)? 0.0f : 1.3f * fireVotes / ((float) (fireVotes + iceVotes)) - 0.65f; // height offset of perlin noise for the temperature map
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				temperature [i, j] = Mathf.PerlinNoise (xOffset + i * xScale, xOffset + j * xScale) - 0.5f + heightOffset;
+			}
+		}
+
+		// set humidity map of room;
+		float [,] humidity = new float[width, height]; // humidity map of room;
+		xOffset = (float) (random.NextDouble ()) * 1000;
+		yOffset = (float) (random.NextDouble ()) * 1000;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				humidity [i, j] = Mathf.PerlinNoise (xOffset + i * xScale, xOffset + j * xScale);
+			}
+		}
+
+
 		// create room
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				if (room [i, j] == wall) {
-					// place walls
-					GameObject wallClone = (GameObject)Instantiate (wallTiles, new Vector3 (i, j, 0.0f), transform.rotation);
+				if (room [i, j] == air) { // set lava/steam/water/floor tiles base on the temperature and humidity
+					if (humidity [i, j] > 0.65f) {
+						GameObject setBlock; // the block to be set in the dungeon
+						if (temperature [i, j] > 0.1f) {
+							setBlock = steamTiles;
+						} else if (temperature [i, j] < -0.1f) {
+							setBlock = iceTiles;
+						} else {
+							setBlock = waterTiles;
+						}
+						GameObject tileClone = (GameObject)Instantiate (setBlock, new Vector3 (i, j, 0.0f), transform.rotation);
+					} else if (humidity [i, j] < 0.35f && temperature [i, j] > 0.1f) {
+						GameObject tileClone = (GameObject)Instantiate (lavaTiles, new Vector3 (i, j, 0.0f), transform.rotation);
+					} else {
+						GameObject tileClone = (GameObject)Instantiate (floorTiles, new Vector3 (i, j, 0.0f), transform.rotation);
+					}
+				}
+
+				if (room [i, j] == wall) { // set the walls and borders of the room
+					// see if there are air tiles in the 3x3 area
+					bool nearAir = false; // there is air tiles nearby
+					for (int row = i - 1; row <= i + 1; row++) {
+						for (int col = j - 1; col <= j + 1; col++) {
+							try {
+								if (room [row, col] == air) {
+									nearAir = true;
+								}
+							} catch (IndexOutOfRangeException) {}
+						}
+					}
+					GameObject tileClone = (GameObject)Instantiate (nearAir? borderTiles : wallTiles, new Vector3 (i, j, 0.0f), transform.rotation);
 				}
 			}
 		}
+
 	}
 
 	// flood filling tiles around chosen point
