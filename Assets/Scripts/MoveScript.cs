f1dd3f38f45b602b5f58e07ce49d7d26ec69fc74
TatQThanh <tqthanh@apcs.vn>
2017-02-09 00:11:59 -0500

    Create new scene to add the function for player to shoot bullet out. Not yet added into dungeon scene. Collision not yet added.

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(10, 10);
+	public Vector2 direction = new Vector2(-1, 0);
+
+	private Vector2 movement;
+	private Rigidbody2D rigidbodyComponent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 2 - Movement
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+
+	void FixedUpdate()
+	{
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+		// Apply movement to the rigidbody
+		rigidbodyComponent.velocity = movement;
+	}
+}
---
+++ Assets/ShotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour {
+
+
+	public int damage = 1;
+
+	public bool isEnemyShot = false;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 10);
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+}
