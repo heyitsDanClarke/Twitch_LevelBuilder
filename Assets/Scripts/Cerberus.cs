567e8148b67d797983aa2b9b9df4b47dc35ffd40
Daniel Clarke <danclarke88@gmail.com>
2017-02-26 14:39:34 -0500

    Cerberus assets (first boss) uploaded. Not integrated into scene yet (working on it). Also moved some scripts and stuff.

---
+++ Assets/Scripts/Cerberus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cerberus : MonoBehaviour {
+
+    Rigidbody2D _rb;
+    Animator _anim;
+
+    public GameObject fireball;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (_rb.velocity.x != 0 || _rb.velocity.y != 0)
+            _anim.SetBool("Moving", true);
+        else
+            _anim.SetBool("Moving", false);
+
+        if (Input.GetKeyDown("j"))
+        {
+            GameObject tempFire = Instantiate(fireball, transform.position, transform.rotation);
+            tempFire.GetComponent<Rigidbody2D>().velocity = Vector2.down;
+        }
+
+    }
+}
