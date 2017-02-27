19cc5ebbd1b975d12cf758b3d8cfb9ee594fa535
Daniel Clarke <danclarke88@gmail.com>
2017-02-26 21:42:05 -0500

    Boss now wanders a little bit, shoots fireballs

--- Assets/Scripts/Cerberus.cs
+++ Assets/Scripts/Cerberus.cs
@@ -6,18 +6,40 @@ public class Cerberus : MonoBehaviour {
 
     Rigidbody2D _rb;
     Animator _anim;
+    float counter;
+    int moveReset;
+    bool paused;
+
+    public float speed;
+    public float damage;
+    public float health;
 
     public GameObject fireball;
 
     void Start()
     {
+        moveReset = Random.Range(1, 3);
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _rb.velocity = ChooseDirection();
     }
 
     void Update()
     {
-        _rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (!paused)
+        {
+            counter += Time.deltaTime;
+            if (counter >= moveReset)
+            {
+                StartCoroutine(PauseFire());
+                moveReset = Random.Range(1, 3);
+                counter = 0;
+            }
+        }
+        else
+        {
+
+        }
         if (_rb.velocity.x != 0 || _rb.velocity.y != 0)
             _anim.SetBool("Moving", true);
         else
@@ -29,5 +51,23 @@ public class Cerberus : MonoBehaviour {
             tempFire.GetComponent<Rigidbody2D>().velocity = Vector2.down;
         }
 
+    }
+
+    Vector2 ChooseDirection()
+    {
+        float x = Random.Range(-1.0f, 1.0f);
+        float y = Random.Range(-1.0f, 1.0f);
+        return new Vector2(x, y);
+    }
+
+    IEnumerator PauseFire()
+    {
+        _rb.velocity = Vector2.zero;
+        paused = true;
+        yield return new WaitForSeconds(1);
+        GameObject tempFire = Instantiate(fireball, new Vector2(transform.position.x, transform.position.y-0.5f), transform.rotation);
+        tempFire.GetComponent<Rigidbody2D>().velocity = Vector2.down;
+        _rb.velocity = ChooseDirection();
+        paused = false;
     }
 }
