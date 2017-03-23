a794fda766ea13ecacf793d0188fb49c667f07ed
Alec Robinson <arobinson.illtemperedklavier@gmail.com>
2017-03-22 23:24:31 -0400

    Added coin motion mechanic.

---
+++ Assets/CoinController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+    public int speed = 2;
+    public float pullRadius = 2;
+    public float pullForce = 10;
+    // Use this for initialization
+    //[HideInInspector]
+    public Rigidbody2D rb2d;
+
+    void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.mass = 1.0f; // mass of coin
+        rb2d.drag = 0.0f; // drag of coin
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 forceDirection = transform.position - Player.Instance.transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, Player.Instance.transform.position, speed * Time.deltaTime);
+        //rb2d.AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+        
+    }
+}
---
+++ Assets/GemController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemController : MonoBehaviour {
+    public float speed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, Player.Instance.transform.position, speed * Time.deltaTime);
+    }
+}
