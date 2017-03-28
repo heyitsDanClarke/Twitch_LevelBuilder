918221b44209fd6918c0e7c48735c11383f1b969
LENOVOPC\Patrick Luk <lukpwh330@yahoo.com.hk>
2017-03-28 12:57:30 -0400

    Added weapon durability timer UI

--- Assets/WeaponTimerScript.cs
+++ Assets/WeaponTimerScript.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponTimerScript : MonoBehaviour {
 
+	public float value; // value of the pie chart
+	public float maxValue; // max value of the pie chart
+
 	// Use this for initialization
 	void Start () {
-		
+		value = 20.0f;
+		maxValue = 20.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// update pie chart
+		value = Mathf.Max(value - Time.deltaTime, 0.0f);
+		transform.FindChild ("Value").GetComponent<Text> ().text = string.Format ("{0:F1}", value);
+		transform.FindChild ("Max Value").GetComponent<Text> ().text = string.Format ("{0:F1}", maxValue);
+
+		//value = float.Parse (transform.FindChild ("Value").GetComponent<Text> ().text);
+		//maxValue = float.Parse(transform.FindChild ("Max Value").GetComponent<Text> ().text);
+
+		transform.FindChild("Pie").GetComponent<Image> ().fillAmount = Mathf.Clamp01(value / maxValue);
 	}
 }
